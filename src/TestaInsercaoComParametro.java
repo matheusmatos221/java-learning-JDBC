7ef69d928345fea36628ebff9b369554a092e9b1
Matheus Matos <matheusmatos.221@hotmail.com>
2022-03-09 14:52:15 -0300

    ADD NEW CLASS TestaInsercaoComParametro.java to add using describe

---
+++ src/TestaInsercaoComParametro.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercaoComParametro {
+    public static void main(String[] args) throws SQLException {
+        String nome = "MOUSE";
+        String descricao = "MOUSE SEM FIO";
+
+        // Cria conexao com banco de dados a partir da factory
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.recuperarConexao();
+
+        // Statement (declaraçao de consulta SQL)
+        Statement statement = connection.createStatement();
+
+        boolean resultado = statement.execute("INSERT INTO PRODUTO (nome, descricao) VALUES" +
+                        " ('" + nome + "', '"+ descricao +"');",
+                Statement.RETURN_GENERATED_KEYS);
+        ResultSet rst = statement.getGeneratedKeys();
+        while (rst.next()){
+            Integer id = rst.getInt(1);
+            System.out.printf("O ID criado foi: %d %n", id);
+        }
+
+        System.out.printf("SQL STATEMENT RESULT: %5b %n", resultado);
+
+    }
+}
