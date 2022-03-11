c0811088e3c1e4a8f190a20b26dc0478b8bf8a35
Matheus Matos <matheus.matos@accurate.com.br>
2022-03-11 14:43:19 -0300

    ADD Produto model

--- src/br/com/matheus/jdbc/modelo/Produto.java
+++ src/br/com/matheus/jdbc/modelo/Produto.java
@@ -1,4 +1,28 @@
 package br.com.matheus.jdbc.modelo;
 
 public class Produto {
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    public Produto(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("O produto criado foi: %d, %s, %s", this.id, this.nome, this.descricao);
+    }
 }
